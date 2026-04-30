b3d51f12d6a0c0eaf716aa286bf8657607f50bbf
Lithesh Udayakumar <lich3184@gmail.com>
2026-04-30 19:59:05 +0530

    removed some comments

--- src/main.c
+++ src/main.c
@@ -16,18 +16,18 @@ int main(void) {
   bool running = true; 
 
   while (running) {
-    int inpCh = getch(); // whatever char you enter is stored here
-    if (inpCh == 'q') { // if the char you entered is 'q'
-      running = false;  // then change running from true to false (hence, exiting)
-    } else {            // if not q
-      clear();          // clear screen
-      printw("Wrong command!"); // print this
-      refresh(); // then refresh
+    int inpCh = getch(); 
+    if (inpCh == 'q') { 
+      running = false;  
     }
+    clear();
+    printw("Chromite - Text Editor");
+    printw("Press q to exit");
+    refresh();
   }
 
-  clear(); // clear screen
-  endwin(); // endwin()
+  clear(); 
+  endwin(); 
 
   return 0;
 }
