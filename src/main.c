0ffd6679a4d9e96f120f3d086f85808e5c95caf7
Lithesh Udayakumar <lich3184@gmail.com>
2026-04-30 19:32:56 +0530

    comments for main.c
    idk why I did this but ok

--- src/main.c
+++ src/main.c
@@ -1,34 +1,33 @@
 #include <ncurses.h>
 #include <stdbool.h>
 
+// this whole function initializes the terminal
 void init_terminal(void) {
-  initscr();
-  cbreak();
-  noecho();
-  keypad(stdscr, TRUE);
+  initscr(); /* Initializes ncurses */
+  cbreak();  /* Disables line buffering (instant input) */
+  noecho();  /* Disable automatic echoing of characters */
+  keypad(stdscr, TRUE); /* Enables special keys (e.g. arrow keys, home key, end key, etc.) */
 }
 
 int main(void) {
-  init_terminal();
+  init_terminal(); 
 
+  // a bool to check if the editor is running or not
   bool running = true; 
 
   while (running) {
-    clear();
-    printw("Chromite Text Editor");
-
-    int inpCh = getch();
-    if (inpCh == 'q') {
-      running = false; 
-    } else {
-      clear();
-      printw("Wrong command!");
-      refresh();
+    int inpCh = getch(); // whatever char you enter is stored here
+    if (inpCh == 'q') { // if the char you entered is 'q'
+      running = false;  // then change running from true to false (hence, exiting)
+    } else {            // if not q
+      clear();          // clear screen
+      printw("Wrong command!"); // print this
+      refresh(); // then refresh
     }
   }
 
-  clear();
-  endwin();
+  clear(); // clear screen
+  endwin(); // endwin()
 
   return 0;
 }
